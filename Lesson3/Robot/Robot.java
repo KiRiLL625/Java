7d2485e655d47c4c6648c749aeefbc333568f0f8
KiRiLL625 <70346582+KiRiLL625@users.noreply.github.com>
2023-04-27 17:29:15 +0300

    Update Robot.java

--- Lesson3/Robot/Robot.java
+++ Lesson3/Robot/Robot.java
@@ -1,1 +1,35 @@
+enum Direction{
+    DOWN,
+    LEFT,
+    UP,
+    RIGHT
+}
 
+public class Robot {
+    private int x;
+    private int y;
+    private Direction direct;
+    public Robot(){
+        this.x = 0;
+        this.y = 0;
+        this.direct = Direction.UP;
+    }
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+    public Direction getDirection(){
+        return direct;
+    }
+    public void setX(int x){
+        this.x = x;
+    }
+    public void setY(int y){
+        this.y = y;
+    }
+    public void setDirection(Direction direct){
+        this.direct = direct;
+    }
+}
