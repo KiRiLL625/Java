85df54864f72aab70b40ec1f18289300a0009577
KiRiLL625 <70346582+KiRiLL625@users.noreply.github.com>
2023-04-27 17:28:26 +0300

    Update Main.java

--- Lesson3/Robot/Main.java
+++ Lesson3/Robot/Main.java
@@ -1,1 +1,9 @@
-
+public class Main {
+    public static void main(String[] args) {
+        Robot robot1 = new Robot();
+        System.out.println(robot1.getX() + " " + robot1.getY() + " " + robot1.getDirection());
+        robot1.setX(5);
+        robot1.setDirection(Direction.LEFT);
+        System.out.println(robot1.getX() + " " + robot1.getY() + " " + robot1.getDirection());
+    }
+}
