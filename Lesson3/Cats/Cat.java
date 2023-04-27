6321fb3a04cab403acc248822b8a0c74678b2c16
KiRiLL625 <70346582+KiRiLL625@users.noreply.github.com>
2023-04-27 17:26:35 +0300

    Update Cat.java

--- Lesson3/Cats/Cat.java
+++ Lesson3/Cats/Cat.java
@@ -1,1 +1,38 @@
-
+public class Cat{
+    private String name;
+    private int age;
+    private String color;
+    private static int sleephours = 10;
+    //breed
+    public Cat(){
+        this.name = "Barsik";
+        this.age = 5;
+        this.color = "grey";
+    }
+    public Cat(String name, int age, String color){
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public String getColor(){
+        return color;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public void setColor(String color){
+        this.color = color;
+    }
+    public static int getSleephours(){
+        return sleephours;
+    }
+}
