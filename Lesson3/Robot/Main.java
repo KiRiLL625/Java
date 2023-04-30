0536f2b583a53cece0badfa20c1d69e042c837fe
KiRiLL625 <70346582+KiRiLL625@users.noreply.github.com>
2023-05-01 01:13:10 +0300

    Update Main.java

--- Lesson3/Robot/Main.java
+++ Lesson3/Robot/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Robot robot1 = new Robot();
+        Robot robot1 = new Robot(); //создаём нового робота
         System.out.println(robot1.getX() + " " + robot1.getY() + " " + robot1.getDirection());
-        robot1.setX(5);
-        robot1.setDirection(Direction.LEFT);
+        //выводим его координаты и направление, выведет: 0 0 UP
+        robot1.setX(5); //устанавливаем координату х = 5
+        robot1.setDirection(Direction.LEFT); //устанавливаем направление влево
         System.out.println(robot1.getX() + " " + robot1.getY() + " " + robot1.getDirection());
+        //выводим его координаты и направление, выведет: 5 0 LEFT
     }
 }
