19e65c06811d4427b64b4886ce052fa56e55e454
KiRiLL625 <70346582+KiRiLL625@users.noreply.github.com>
2023-05-15 15:07:14 +0300

    Create Animal.java

---
+++ Lesson6/ObjectsMassive/Animal.java
@@ -0,0 +1,24 @@
+//обычный класс Animal
+public class Animal{
+    private String name;
+    private int age;
+    public Animal(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    public Animal(){
+        this("Boba", 3);
+    }
+    public void say(){
+        System.out.println("Animal says");
+    }
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void play(){
+        System.out.println("I'm playing");
+    }
+}
